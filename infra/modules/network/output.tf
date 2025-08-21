bb5c5ccb46f95e35591d1da0cd3d8bf0ea8b1fea
Jose Cuendes <jcuendes86@gmail.com>
2025-08-21 20:13:09 +0200

    feat: Refactor network module by removing subnetwork resources and updating related configurations

--- infra/modules/network/output.tf
+++ infra/modules/network/output.tf
@@ -7,13 +7,3 @@ output "network_name" {
   description = "VPC Network name"
   value       = google_compute_network.custom-network.name
 }
-
-output "subnetwork" {
-  description = "VPC Subnetwork object"
-  value       = google_compute_subnetwork.custom-subnetwork
-}
-
-output "subnetwork_self_link" {
-  description = "VPC Subnetwork self link"
-  value       = google_compute_subnetwork.custom-subnetwork.self_link
-}
